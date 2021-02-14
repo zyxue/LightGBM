41bef460acddbcd6ce78aa5537d25b47e43b97ce
zyxue <alfred532008@gmail.com>
2021-02-13 21:34:29 -0800

    more fix to imports in sklearn

--- python-package/lightgbm/sklearn/__init__.py
+++
@@ -1,9 +0,0 @@
-from .classifier import LGBMClassifier
-from .doc import (
-    _lgbmmodel_doc_custom_eval_note,
-    _lgbmmodel_doc_fit,
-    _lgbmmodel_doc_predict,
-)
-from .model import LGBMModel
-from .ranker import LGBMRanker
-from .regressor import LGBMRegressor
--- python-package/lightgbm/sklearn/model.py
+++ python-package/lightgbm/sklearn/model.py
@@ -18,11 +18,15 @@ from ..compat import (
     pd_DataFrame,
 )
 from ..engine import train
+from .classifier import LGBMClassifier
 from .doc import (
     _lgbmmodel_doc_custom_eval_note,
     _lgbmmodel_doc_fit,
     _lgbmmodel_doc_predict,
 )
+from .model import LGBMModel
+from .ranker import LGBMRanker
+from .regressor import LGBMRegressor
 
 
 class LGBMModel(_LGBMModelBase):
